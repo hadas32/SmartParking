dd1b8d0e7c7025612cb9fbafeeb5a43ffcb49ecc
“hadas32 <hadasc917@gmail.com>
2026-04-13 23:30:30 +0300

    Initial commit - Clean SmartParking API working project

---
+++ backend/SmartParking.Core/DTOs/SpotDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartParking.Core.DTOs
+{
+    public class SpotDTO
+    {
+        public int Id { get; set; }
+        public int Spot_number { get; set; }
+        public bool Is_occupied { get; set; }
+        public ParkingDTO Parking { get; set; }
+    }
+}
---
+++ backend/SmartParking/Program.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer; 
+using Microsoft.IdentityModel.Tokens; 
+using Microsoft.OpenApi.Models;
+using SmartParking;
+using SmartParking.Core;
+using SmartParking.Core.Repositories;
+using SmartParking.Core.Services;
+using SmartParking.Data.Repoistories;
+using SmartParking.Service;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:8081")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+// --- הגדרות JWT ---
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddScoped<ICarService, CarService>();
+builder.Services.AddScoped<ICarRepository, CarsRepository>();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddScoped<IParkingService, ParkingService>();
+builder.Services.AddScoped<IParkingRepository, ParkingsRepository>();
+builder.Services.AddScoped<ISpotService, SpotService>();
+builder.Services.AddScoped<ISpotRepository, SpotsRepository>();
+builder.Services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
+
+
+app.UseAuthentication(); 
+app.UseAuthorization();  
+
+app.MapControllers();
+
+app.Run();
